b47ac42d00d4986d46a153f697b39ebff45a7021
XEZ123 <discordraspberrypi@gmail.com>
2021-10-07 15:21:54 +0100

    Welcome message change
    removed the graphical welcome and leave message and decided to make a randomized welcome message - just did the basics of it

---
+++ events/welcome.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const { welcomechannel } = require('../config/constants/channel.json');
+
+
+module.exports = (client) => {
+    client.on("guildMemberAdd", (member) => {
+        var welcometext = [
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+        ]
+        var randomtextmessage = Math.floor(Math.random() * welcometext.length);
+        const channel = member.guild.channels.cache.get(welcomechannel);
+        channel.send(randomtextmessage);
+    });
+};
